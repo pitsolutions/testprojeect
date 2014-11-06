03c811e053bf34f1b62803f2cbb22d660a3d41b4
sivapits <sivaprasad@pitsolutions.com>
2014-11-06 14:19:21 +0530

    Initial commit

---
+++ typo3conf/LocalConfiguration.php
@@ -0,0 +1,66 @@
+<?php
+return array(
+	'BE' => array(
+		'debug' => FALSE,
+		'explicitADmode' => 'explicitAllow',
+		'installToolPassword' => '$P$CraJTJX78oqbvSEgRoVrRROaZYb0fA.',
+		'loginSecurityLevel' => 'rsa',
+	),
+	'DB' => array(
+		'database' => 'vienna_sightseeing',
+		'extTablesDefinitionScript' => 'extTables.php',
+		'host' => 'localhost',
+		'password' => '',
+		'socket' => '',
+		'username' => 'root',
+	),
+	'EXT' => array(
+		'extConf' => array(
+			'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+			'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+			'templavoila' => 'a:3:{s:7:"enable.";a:3:{s:13:"oldPageModule";s:1:"0";s:19:"selectDataStructure";s:1:"0";s:15:"renderFCEHeader";s:1:"0";}s:9:"staticDS.";a:3:{s:6:"enable";s:1:"0";s:8:"path_fce";s:27:"fileadmin/templates/ds/fce/";s:9:"path_page";s:28:"fileadmin/templates/ds/page/";}s:13:"updateMessage";s:1:"0";}',
+		),
+	),
+	'EXTCONF' => array(
+		'lang' => array(
+			'availableLanguages' => array(),
+		),
+	),
+	'FE' => array(
+		'activateContentAdapter' => FALSE,
+		'debug' => FALSE,
+		'loginSecurityLevel' => 'rsa',
+	),
+	'GFX' => array(
+		'jpg_quality' => '80',
+	),
+	'SYS' => array(
+		'caching' => array(
+			'cacheConfigurations' => array(
+				'extbase_object' => array(
+					'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+					'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+					'groups' => array(
+						'system',
+					),
+					'options' => array(
+						'defaultLifetime' => 0,
+					),
+				),
+			),
+		),
+		'clearCacheSystem' => FALSE,
+		'compat_version' => '6.2',
+		'devIPmask' => '',
+		'displayErrors' => 0,
+		'enableDeprecationLog' => FALSE,
+		'encryptionKey' => '1bd2a4947e9ead2a9e49e255965aa6376150c47f258cd4444dc65318fa18918a0bcb6e79dce573b33a2c3ebcf6031409',
+		'isInitialInstallationInProgress' => FALSE,
+		'sitename' => 'Vienna Sightseeing',
+		'sqlDebug' => 0,
+		'systemLogLevel' => 2,
+		't3lib_cs_convMethod' => 'mbstring',
+		't3lib_cs_utils' => 'mbstring',
+	),
+);
+?>
